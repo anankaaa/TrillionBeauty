fe19627282876711f5e0c156e4f2566a56d8a38e
Anankaaa <hegyaljai83@hotmail.com>
2023-10-20 10:39:27 +0200

    first commit

---
+++ TrillionBeauty/src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  videoUrl: string =
+    './../assets/stock-video-cosmetic-cream-skin-care-moisturizer-face.mp4';
+}
